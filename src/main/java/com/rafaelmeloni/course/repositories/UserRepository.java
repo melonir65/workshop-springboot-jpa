f1474370410199697d53ce9fe815ef29d112a5f7
Rafael Meloni <rafaelmeloni@gmail.com>
2025-03-31 21:36:45 -0300

    JPA repository, dependency injection, database seeding

---
+++ src/main/java/com/rafaelmeloni/course/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.rafaelmeloni.course.repositories;
+
+import com.rafaelmeloni.course.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+
+}
