57b8e1be9959c803ad060ce5cc3b4ae07ce3507d
Rafael Meloni <rafaelmeloni@gmail.com>
2025-04-06 22:52:46 -0300

    Exception handling - findById

--- src/main/java/com/rafaelmeloni/course/services/UserService.java
+++ src/main/java/com/rafaelmeloni/course/services/UserService.java
@@ -2,6 +2,7 @@ package com.rafaelmeloni.course.services;
 
 import com.rafaelmeloni.course.entities.User;
 import com.rafaelmeloni.course.repositories.UserRepository;
+import com.rafaelmeloni.course.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id){
         Optional<User> obj = userRepository.findById(id);
-        return obj.get();
+        return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 
     }
 
